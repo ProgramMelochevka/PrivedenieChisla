f86d15920b1c027a7fc8308444cf27d2c3869125
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-10-03 21:10:03 +0300

    Примеры педевода чисел из одних в другие

---
+++ src/by/home/maxzzzit/PrivedenieChisla.java
@@ -0,0 +1,20 @@
+package by.home.maxzzzit;
+
+public class PrivedenieChisla {
+	public static void main(String[]args) {
+		int a=54;
+		double d;
+		d=(double)a;
+		System.out.println("Перевод числа из целого (int) в вещественное "+d);
+		
+		long lg=564392018;
+		float f;
+		f=(float)lg;
+		System.out.println("Перевод числа из целого длинного (long) в вещественное "+f);
+		
+		double db = 3654.7032;
+		int i;
+		i=(int)db;
+		System.out.println("Перевод числа из вещественного (double) в целое "+i);
+	}
+}
